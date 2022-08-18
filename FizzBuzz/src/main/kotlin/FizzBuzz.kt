c029dc920d3023ae00bb53e2fdead8148291eb1b
Carlos Bastos <carlos.rbastoss@gmail.com>
2022-08-18 17:33:47 -0300

    Programando FizzBuzz

---
+++ FizzBuzz/src/main/kotlin/FizzBuzz.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var num = 0
+
+    do {
+        num++
+
+        if (num % 3 == 0 && num % 5 == 0) {
+            println("FizzBuzz")
+        } else if (num % 3 == 0) {
+            println("Fizz")
+        } else if (num % 5 == 0) {
+            println("Buzz")
+        } else {
+            println("$num")
+        }
+    } while (num in 1..99)
+}
+
+
+
+
+
